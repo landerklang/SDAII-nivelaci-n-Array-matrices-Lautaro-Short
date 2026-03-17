fda0813d1584e647eae8cbca69d7222d1dda522f
¨lautaro <shortlauty35@gmail.com>
2026-03-17 09:43:16 -0300

    first commit

---
+++ tp-py/main.py
@@ -0,0 +1,24 @@
+Cantidadalumno=int(input("ingrese la cantidad de alumnos a añadir: "))
+notamayor=[]
+promedio=0
+i=0
+Totalalumno=[]
+
+while i!=Cantidadalumno:
+
+    print(i)
+    alumno=[]
+    alumno.append(input("Nombre: "))
+    alumno.append(int(input("Edad: ")))
+    alumno.append(int(input("Nota: ")))
+    Totalalumno.append(alumno)
+    notamayor.append(Totalalumno[i][2])
+    Totalnota=promedio+alumno[2]
+    i=i+1
+if Cantidadalumno==0:
+    print("no hay alumnos")
+notamayor.sort(reverse=True)
+promedio=Totalnota/Cantidadalumno
+print("Alumno: ",Totalalumno)
+print("Notas ordenadas de mayor a menor: ",notamayor)
+print("Promedio:",promedio)
